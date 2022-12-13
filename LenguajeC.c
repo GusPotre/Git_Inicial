e9701fc1e76dd8946cd3371f5c32d3a053aff7e7
Gus <geuspotrero080103@gmail.com>
2022-12-13 15:57:34 -0600

    Adición de archivo C

---
+++ LenguajeC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    printf("Ingrese dos numeros: ");
+    scanf("%i %i", &x, &y);
+    printf("Numero 1: %i\n", x);
+    printf("Numero 2: %i", y);
+
+    return 0;
+}
