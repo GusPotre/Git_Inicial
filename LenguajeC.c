450faa496e8e91c22ea90cefb460b9b2108c5944
Gus <geuspotrero080103@gmail.com>
2022-12-13 16:28:07 -0600

    Función suma en C

--- LenguajeC.c
+++ LenguajeC.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+int suma(int num1, int num2);
+
 int main()
 {
+    int sumar;
     int x, y;
     printf("Ingrese dos numeros: ");
     scanf("%i %i", &x, &y);
     printf("Numero 1: %i\n", x);
     printf("Numero 2: %i", y);
 
+    sumar = suma(x, y);
+    printf("La suma de los dos numeros es: %i", suma);
     return 0;
+}
+
+int suma(int num1, int num2)
+{
+    int suma = 0;
+    suma = num1 + num2;
+    return suma;
 }
