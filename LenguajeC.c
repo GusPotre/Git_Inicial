6c08f16b664a221405689aa837562c0fdcb6d3e8
Gus <geuspotrero080103@gmail.com>
2022-12-13 16:20:04 -0600

    Br: Imprimir nombre en C

--- LenguajeC.c
+++ LenguajeC.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+void saludo(int dim, char name[dim]);
+
 int main()
 {
+    char nombre[20];
     int x, y;
     printf("Ingrese dos numeros: ");
     scanf("%i %i", &x, &y);
     printf("Numero 1: %i\n", x);
-    printf("Numero 2: %i", y);
+    printf("Numero 2: %i\n\n", y);
+
+    printf("Ingrese su nombre: ");
+    scanf("%s", nombre);
+    saludo(20, nombre);
 
     return 0;
+}
+
+void saludo(int dim, char name[dim])
+{
+    printf("Hola %s", name);
 }
