e8733409c92cce217b094e68e18ec79bd8caea77
Gus <geuspotrero080103@gmail.com>
2022-12-13 19:26:57 -0600

    Comparación con condicional IF

---
+++ Condicional.py
@@ -0,0 +1,14 @@
+num1 = 3
+num2 = 7
+
+if num1 < num2:
+  print("El número es menor")
+if num1 > num2:
+  print("El número es mayor")
+else:
+  print("Los números son iguales")
+
+x = 1
+while x < 10:
+  print("Hola")
+  x = x + 1
