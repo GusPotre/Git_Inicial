bf8cca77f87565eb4481556f9ce01a823b98d4fb
Gus <geuspotrero080103@gmail.com>
2022-12-13 21:12:00 -0600

    Nueva rama

---
+++ suma.py
@@ -0,0 +1,15 @@
+print("Hola Mundo")
+
+num1 = 3
+num2 = 5
+suma = 0 
+suma = num1+ num2
+print(suma)
+
+num3 = 4
+num4 = 8
+resta = 0
+resta = num3 - num4
+print(resta)
+
+# Python puede trabajar con una placa de desarrollo
